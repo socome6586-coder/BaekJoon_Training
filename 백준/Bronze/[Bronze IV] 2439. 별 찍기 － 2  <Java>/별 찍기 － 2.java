0f8af6cfcf5241ae8545ff932d973c9544297ea7
Seok <socome6586@gmail.com>
2026-04-15 14:03:43 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 316 ms, Memory: 21796 KB -BaekjoonHub

---
+++ 백준/Bronze/[Bronze IV] 2439. 별 찍기 － 2  <Java>/별 찍기 － 2.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		Scanner        sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= n-i; j++) {
+				System.out.print(" ");
+			} 
+			for (int j = 0; j < i; j++) {
+				System.out.print("*");	
+			}
+			System.out.println();
+		}
+		
+		bw.flush();
+		br.close();
+	}
+}
